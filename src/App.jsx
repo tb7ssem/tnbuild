8c5a5edcd689b2fcb07f2e9bbd6a81c8faed4ca7
Houssem Tabboubi <124980612+tb7ssem@users.noreply.github.com>
2024-09-29 20:08:56 +0100

    commit 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AdminDashboard from './pages/AdminDashboard';
 import Login from './pages/Login';
 import Header from './components/Header';
@@ -14,11 +14,11 @@ const App = () => {
             <Login/>
                 <div className="app">
                     <Header />
-                    <Switch>
+                    <Routes>
                         <Route path="/login" component={Login} />
                         <Route path="/admin-dashboard" component={AdminDashboard} />
                         <Route path="/" component={Login} /> {/* Default route */}
-                    </Switch>
+                    </Routes>
                 </div>
             </Router>
         </AuthProvider>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../context/AuthContext.jsx';
 import './Login.css'; // Ensure this file exists
 
 const Login = () => {
